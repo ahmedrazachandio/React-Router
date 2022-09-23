45507564d314dd9919d942fa1c4cd34e2918d470
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-09-23 22:31:53 +0500

    class code push

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <router>
+        <Routes>
+          
+        </Routes>
+      </router>
+     Ahmed Raza
+    </div>
+  );
+}
+
+export default App;
